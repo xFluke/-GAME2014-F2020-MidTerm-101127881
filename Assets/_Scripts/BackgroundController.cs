d0e619339e2f9895f0d1055ad4ed9a4996d6256f
xFluke <mikoman1124@gmail.com>
2020-10-24 22:44:10 -0400

    Initial Commit
    (Only read about having to make commits to Github AFTER modifying all the code oops)

---
+++ Assets/_Scripts/BackgroundController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    public float horizontalSpeed;
+    public float horizontalBoundary;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Move();
+        _CheckBounds();
+    }
+
+    private void _Reset()
+    {
+        transform.position = new Vector3(horizontalBoundary, 0.0f);
+    }
+
+    private void _Move()
+    {
+        transform.position -= new Vector3(horizontalSpeed, 0.0f) * Time.deltaTime;
+    }
+
+    private void _CheckBounds()
+    {
+        // if the background is lower than the bottom of the screen then reset
+        if (transform.position.x <= -horizontalBoundary)
+        {
+            _Reset();
+        }
+    }
+}
---
+++ Assets/_Scripts/EnemyController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float verticalSpeed;
+    public float vertictalBoundary;
+    public float direction;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Move();
+        _CheckBounds();
+    }
+
+    private void _Move()
+    {
+        transform.position += new Vector3(0.0f, verticalSpeed * direction * Time.deltaTime, 0.0f);
+    }
+
+    private void _CheckBounds()
+    {
+        // check right boundary
+        if (transform.position.y >= vertictalBoundary)
+        {
+            direction = -1.0f;
+        }
+
+        // check left boundary
+        if (transform.position.y <= -vertictalBoundary)
+        {
+            direction = 1.0f;
+        }
+    }
+}
